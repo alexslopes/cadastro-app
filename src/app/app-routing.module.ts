115abee67566f2cb9770b81ecd8b497aef9d77fb
alexs <alexstlopes@gmail.com>
2022-03-07 23:05:14 -0300

    Adicionado template e layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './layout/layout.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LayoutComponent, children: [
+    { path: 'home', component: HomeComponent}
+  ]}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HomeComponent } from './home/home.component';
+import { TemplateModule } from './template/template.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    TemplateModule,
     AppRoutingModule
   ],
   providers: [],
