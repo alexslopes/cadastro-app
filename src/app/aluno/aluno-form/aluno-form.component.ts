fafe6a58a6c9af1dd674ff82a0fa35d80867276a
alexs <alexstlopes@gmail.com>
2022-03-14 21:12:20 -0300

    Cadastro de aluno funcional

--- src/app/aluno/aluno-form/aluno-form.component.ts
+++ src/app/aluno/aluno-form/aluno-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AlunoService } from 'src/app/aluno.service';
+import { Aluno } from '../aluno';
 
 @Component({
   selector: 'app-aluno-form',
@@ -7,9 +11,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlunoFormComponent implements OnInit {
 
-  constructor() { }
+  aluno: Aluno;
+  success: boolean = false;
+  errors: String[];
+  id: number;
+
+  constructor( private service: AlunoService,
+               private router: Router,
+               private activatedRoute: ActivatedRoute ) {
+    this.aluno = new Aluno();
+  }
 
   ngOnInit(): void {
+    let params: Observable<Params> = this.activatedRoute.params;
+    params.subscribe( urlParams => {
+      this.id = urlParams['id'];
+      if(this.id) {
+        this.service.getAlunoById(this.id)
+        .subscribe(
+          response => this.aluno = response,
+          errorresponse => this.aluno = new Aluno())
+      }
+
+    })
+
+    console.log(this.aluno.nome)
   }
+
+  voltarParaListagem() {
+    this.router.navigate(['/aluno/lista'])
+  }
+
+  clicar() {
+    console.log(this.aluno);
+  }
+
+  onSubmit() {
+    if(this.id) {
+      this.service.atualizar(this.aluno)
+      .subscribe( response =>
+        {this.success = true;
+        this.errors = null;
+      }, errorResponse => {
+        this.errors = [ 'Erro ao atualizar o cliente.']
+      })
+    } else {
+
+    this.service.salvar(this.aluno).subscribe(
+      (response) => { this.success = true;
+        this.errors = null;
+        this.aluno = response;
+      }, errorResponse => {
+        this.success = false;
+        this.errors = errorResponse.error.errors;
+      })
+  }
+}
 
 }
